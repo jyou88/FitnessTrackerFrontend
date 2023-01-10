cb93e066f68dd28ebd968e40992bb0a290b46167
Jae You <jyou88@gmail.com>
2023-01-10 10:04:42 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/Navbar/index.js";
-import { Home } from "./components/Home/index.js";
-
-import Routines from "./components/Routines";
-import Activities from "./components/Activities";
-import MUILogin from "./components/Login/MUILogin.js";
-import MyRoutines from "./components/MyRoutines/index.js";
-import MUIRegister from "./components/Register/MUIRegister.js";
-
-import UserRoutines from "./components/UserRoutines/index.js";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home token={token} setToken={setToken} />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/routines" element={<Routines />} />
-        <Route path="/myroutines" element={<MyRoutines token={token} />} />
-        <Route path="/login" element={<MUILogin />} />
-        <Route path="/register" element={<MUIRegister />} />
-        <Route path="/userroutines" element={<UserRoutines />} />
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
